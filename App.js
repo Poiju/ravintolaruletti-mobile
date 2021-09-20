4630288675140346a654956c6054c51ad9fddcd7
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2021-09-20 13:06:28 +0300

    Get all places data

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const BASE_PLACES_URL = 'https://open-api.myhelsinki.fi/v1/places/'
+
 export default function App() {
+  const [places, setPlaces] = useState('Loading...')
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    load()
+  }, [])
+
+  async function load() {
+    try {
+      const response = await fetch(BASE_PLACES_URL)
+      const result = await response.json()
+
+      if (response.ok) {
+        setPlaces(result)
+      } else {
+        setErrorMessage(result.message)
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{places}</Text>
+      <Text>{errorMessage}</Text>
       <StatusBar style="auto" />
     </View>
   );
