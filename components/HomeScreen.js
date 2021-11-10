3ac33c76c45ebb381c759460b74643f050501d69
anttomi <tomi.salo@myy.haaga-helia.fi>
2021-11-10 14:58:33 +0200

    API fucntions to new component

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -6,15 +6,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import getLocation from './Location';
 import Slideshow from 'react-native-image-slider-show';
+import getRestaurants from './RestaurantAPI';
 //import { API_TOKEN } from 'react-native-dotenv'
 
-// Google Places API call parameters
-const API_KEY = ""
-// Nearby Search
-const TYPE = 'restaurant'
-const RADIUS = '500' // meters
-// Photo max width
-const PHOTO_WIDTH = '400'
+
+
 //console.log(API_KEY)
 const Stack = createStackNavigator();
 
@@ -22,68 +18,16 @@ export default function HomeScreen({ navigation }) {
   const [restaurants, setRestaurants] = useState([])
 
   useEffect(() => {
-    loadRestaurants()
+    fetchRestaurants()
     generateBoxShadowStyle(0, 5, '#9aa0b9', 0.05, 13, 20, '#9aa0b9')
-
+    console.log(restaurants)
   }, [])
 
-  const loadRestaurants = async () => {
-    try {
-      const loc = await getLocation();
-      const response = await fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + loc.latitude + '%2C' + loc.longitude + '&radius=' + RADIUS + '&type=' + TYPE + '&key=' + API_KEY)
-      const result = await response.json()
 
-      if (response.ok) {
-        console.log('Number of restaurants fetched: ' + result.results.length)
+  const fetchRestaurants = async () => {
 
-        // Load photos and filter out restaurants without any
-        setRestaurantsWithPhotos(result.results)
-      } else {
-        console.log("RESPONSE NOT OK Couldn't load restaurants: " + result.message)
-      }
-    } catch (error) {
-      console.log("ERROR Couldn't load restaurants: " + error.message)
-    }
-  }
-
-  const setRestaurantsWithPhotos = async (data) => {
-    //Filter all restaurants from data that have photos
-    let filteredRestaurants = data.filter(restaurant => restaurant.photos != undefined)
-    //Promise.all called to convert an array of promises to a single promise before awaiting it, so it can be awaited in the first place
-    let restaurantPhotos = await Promise.all(filteredRestaurants.map( (restaurant) => {
-      return restaurant.photos = loadPhotos(restaurant.place_id)
-      
-    }))
-
-  setRestaurants(filteredRestaurants)
-  }
-
-  const loadPhotos = async (placeId) => {
-    try {
-      // Google Places API Search (used for fetching restaurants) returns only a single photo.
-      // We need to additionally use Place Details to get up to 10 photos per restaurant (API limitation)
-      const fields = 'photos'
-      const placeDetailUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=${fields}&key=${API_KEY}`
-
-      // Fetch photo array
-      const response = await fetch(placeDetailUrl)
-
-      if (!response.ok) {
-        throw new Error('HTTP ERROR! status: ' + response.status)
-      }
-
-      const result = await response.json()
-      const photoRefArray = result.result.photos
-
-      // Create new array with direct links to photos
-      const photoUrls = photoRefArray.map(photoRef => {
-        return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=${PHOTO_WIDTH}&photo_reference=${photoRef.photo_reference}&key=${API_KEY}`
-      })
-
-      return photoUrls
-    } catch (error) {
-      console.log('ERROR loading photos: ' + error)
-    }
+     let data = await getRestaurants()
+     setRestaurants(data)
   }
 
   // Necessary for unified iOS and Android box shadow
